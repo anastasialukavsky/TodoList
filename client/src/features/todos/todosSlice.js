564873906e9c75c32e02839566adaf65fac41da5
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-05-13 09:10:15 -0400

    axios refactor

--- client/src/features/todos/todosSlice.js
+++ client/src/features/todos/todosSlice.js
@@ -1,25 +1,18 @@
+import axios from "axios";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchTodosAsync = createAsyncThunk("todos/fetchAll", async () => {
-  const response = await fetch("http://localhost:8080/api/todos");
-  const data = await response.json();
+  const { data } = await axios.get("http://localhost:8080/api/todos");
   return data;
 });
 
 export const addTodoAsync = createAsyncThunk(
   "todos/addTodo",
   async ({ assignee, taskName }) => {
-    const response = await fetch("http://localhost:8080/api/todos", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        assignee,
-        taskName,
-      }),
+    const { data } = await axios.post("http://localhost:8080/api/todos", {
+      assignee,
+      taskName,
     });
-    const data = await response.json();
     return data;
   }
 );
@@ -28,10 +21,7 @@ export const todosSlice = createSlice({
   name: "todos",
   initialState: [],
   reducers: {
-    // All our data is async so our logic does not go here
-    // addTodo: (state, action) => {
-    //   state.push(action.payload);
-    // },
+    // All our data is async so our logic does not go here, but in the extraReducers
   },
   extraReducers: (builder) => {
     builder.addCase(fetchTodosAsync.fulfilled, (state, action) => {
@@ -42,9 +32,6 @@ export const todosSlice = createSlice({
     });
   },
 });
-
-// Not needed as we are using extraBuilders
-// export const { addTodo } = todosSlice.actions;
 
 export const selectTodos = (state) => state.todos;
 
