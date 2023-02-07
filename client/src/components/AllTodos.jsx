bc3c997edbb4a6526f83074f13d3791f61107298
ruralFluffyDill <stacylukavsky@gmail.com>
2023-02-06 22:22:04 -0500

    main view form setup

--- client/src/components/AllTodos.jsx
+++ client/src/components/AllTodos.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllTodos, selectAllTodos } from "../slices/todos/todosSlice";
+import {
+  createSingleTodo,
+  selectSingleTodo,
+} from "../slices/todos/singleTodoSlice";
 
 export default function AllTodos(props) {
   const dispatch = useDispatch();
+  const allTodos = useSelector(selectAllTodos);
+  const singleTodo = useSelector(selectSingleTodo);
 
   useEffect(() => {
     dispatch(fetchAllTodos());
-  }, [dispatch]);
+  }, [dispatch, singleTodo]);
 
-  const allTodos = useSelector(selectAllTodos);
 
   function todosList() {
     if (allTodos.length > 0)
@@ -26,9 +31,59 @@ export default function AllTodos(props) {
       });
   }
 
+  function FormView() {
+    const [taskName, setTaskName] = useState("");
+    const [assignee, setAssignee] = useState("");
+    const [isInvalid, setIsInvalid] = useState(false);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (!taskName || !assignee) {
+        setIsInvalid(true);
+        return;
+      }
+      setTaskName("");
+      setAssignee("");
+      dispatch(createSingleTodo({ taskName, assignee }));
+    };
+    const handleDelete = (event) => {
+      event.preventDefault();
+      setTaskName("");
+      setAssignee("");
+    };
+
+    return (
+      <form className="main-view-form">
+        <label htmlFor="taskName">Task Name</label>
+        <input
+          type="text"
+          name="taskName"
+          id="taskName"
+          value={taskName}
+          onChange={(event) => setTaskName(event.target.value)}
+        />
+        {isInvalid && taskName === "" && <p>Must enter the task</p>}
+
+        <label htmlFor="assignee">Assign To:</label>
+        <input
+          type="text"
+          name="assignee"
+          id="assignee"
+          value={assignee}
+          onChange={(event) => setAssignee(event.target.value)}
+        />
+        {isInvalid && assignee === "" && <p>Must enter your assignee name</p>}
+        <button onClick={handleSubmit}>Submit</button>
+        <br />
+        <button onClick={handleDelete}>Delete</button>
+      </form>
+    );
+  }
+
   return (
     <div>
       <ul>{todosList()}</ul>
+      <ul>{FormView()}</ul>
     </div>
   );
 }
